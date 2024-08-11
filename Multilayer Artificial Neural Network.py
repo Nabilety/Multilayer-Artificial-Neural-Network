7af488dbeeba21c06e4fff7e89a5c9c2bc811ae1
Nabilety <37532159+Nabilety@users.noreply.github.com>
2024-08-11 13:16:57 +0200

    Multilayer Artificial Neural Network from Scratch - Implement Deep Neural Network

--- Multilayer Artificial Neural Network.py
+++ Multilayer Artificial Neural Network.py
@@ -43,7 +43,7 @@ plt.show()
 # Following will split dataset such that 55000 images are used for training, 5000 for images for validation and
 # 10000 images for testing
 X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=10000, random_state=123, stratify=y)
-X_train, X_valid, y_train, y_valid = train_test_split(X_temp, y_temp, test_size=5000, random_state=123, stratify=y)
+X_train, X_valid, y_train, y_valid = train_test_split(X_temp, y_temp, test_size=5000, random_state=123, stratify=y_temp)
 
 ##########################
 ### MODEL
@@ -163,3 +163,115 @@ class NeuralNetMLP:
 model = NeuralNetMLP(num_features=28*28,
                      num_hidden=50,
                      num_classes=10)
+
+# Coding the neural network training loop
+
+# Defining data loaders:
+
+num_epochs = 50
+minibatch_size = 100
+
+# Function taking our dataset and divides it into mini-batches of desired size for stochastic gradient descent training
+def minibatch_generator(X, y, minibatch_size):
+    indices = np.arange(X.shape[0])
+    #print(indices)
+    np.random.shuffle(indices)
+    #print("SHOOOOOO")
+    #print(indices.shape[0])
+    for start_idx in range(0, indices.shape[0] - minibatch_size
+                           + 1, minibatch_size):
+        batch_idx = indices[start_idx:start_idx + minibatch_size]
+        yield X[batch_idx], y[batch_idx]
+
+# iterate over training epochs
+for i in range(num_epochs):
+    # iterate over minibatches
+    minibatch_gen = minibatch_generator(X_train, y_train, minibatch_size)
+    for X_train_mini, y_train_mini in minibatch_gen:
+        break
+    break
+print(X_train_mini.shape)
+print(y_train_mini.shape)
+
+
+# Defining a function to compute the loss and accuracy
+def mse_loss(targets, probas, num_labels=10):
+    onehot_targets = int_to_onehot(targets, num_labels=num_labels)
+    return np.mean((onehot_targets - probas)**2)
+def accuracy(targets, predicted_labels):
+    return np.mean(predicted_labels == targets)
+
+_, probas = model.forward(X_valid)
+mse = mse_loss(y_valid, probas)
+print(f'Initial validation MSE: {mse:.1f}')
+
+predicted_labels = np.argmax(probas, axis=1)
+acc = accuracy(y_valid, predicted_labels)
+print(f'Initial validation accuracy: {acc*100:.1f}%')
+
+
+# Compute MSE and accuracy incrementally by iterating over the dataset one mini-batch at a time
+# to be more memory-efficient, due to large matrix multiplication using forward pass.
+def compute_mse_and_acc(nnet, X, y, num_labels=10, minibatch_size=100):
+    mse, correct_pred, num_examples = 0., 0, 0
+    minibatch_gen = minibatch_generator(X, y, minibatch_size)
+    for i, (features, targets) in enumerate(minibatch_gen):
+        _, probas = nnet.forward(features)
+        predicted_labels = np.argmax(probas, axis=1)
+        onehot_targets = int_to_onehot(targets, num_labels=num_labels)
+        loss = np.mean((onehot_targets - probas)**2)
+        correct_pred += (predicted_labels == targets).sum()
+        num_examples += targets.shape[0]
+        mse += loss
+    mse = mse/i
+    acc = correct_pred/num_examples
+    return mse, acc
+
+mse, acc = compute_mse_and_acc(model, X_valid, y_valid)
+print(f'Initial valid MSE: {mse:.1f}')
+print(f'Initial valid accuracy: {acc*100:.1f}%')
+
+def train(model, X_train, y_train, X_valid, y_valid, num_epochs, learning_rate=0.1):
+    epoch_loss = []
+    epoch_train_acc = []
+    epoch_valid_acc = []
+
+    for e in range(num_epochs):
+        # iterate over minibatches
+        minibatch_gen = minibatch_generator(X_train, y_train, minibatch_size)
+        for X_train_mini, y_train_mini in minibatch_gen:
+            #### Compute outputs ####
+            a_h, a_out = model.forward(X_train_mini)
+
+            #### Comptue gradients ####
+            d_loss__d_w_out, d_loss__d_b_out, d_loss__d_w_h, d_loss__d_b_h = model.backward(X_train_mini,
+                                                                                            a_h, a_out,
+                                                                                            y_train_mini)
+            #### Update weights ####
+            model.weight_h -= learning_rate * d_loss__d_w_h
+            model.bias_h -= learning_rate * d_loss__d_b_h
+            model.weight_out -= learning_rate * d_loss__d_w_out
+            model.bias_out -= learning_rate * d_loss__d_b_out
+
+        #### Epoch Logging ####
+        train_mse, train_acc = compute_mse_and_acc(model, X_train, y_train)
+        valid_mse, valid_acc = compute_mse_and_acc(model, X_valid, y_valid)
+        train_acc, valid_acc = train_acc*100, valid_acc*100
+        epoch_train_acc.append(train_acc)
+        epoch_valid_acc.append(valid_acc)
+        epoch_loss.append(train_mse)
+        print(f'Epoch: {e+1:03d}/{num_epochs:03d} '
+              f'| Train MSE: {train_mse:.2f} '
+              f'| Train Acc: {train_acc:.2f}% '
+              f'| Valid Acc: {valid_acc:.2f}%')
+    return epoch_loss, epoch_train_acc, epoch_valid_acc
+
+# train our model for 50 epochs
+np.random.seed(123) # for the training set shuffling
+epoch_loss, epoch_train_acc, epoch_valid_acc = train(model, X_train, y_train, X_valid, y_valid, num_epochs=50, learning_rate=0.1)
+
+# In general training Deep Neural Networks are relatively expensive compared to other models.
+# So we stop it early in certain circumstances and start over with different hyperparameter settings.
+# If we find that it increasingly tends to overfit the training data (noticeable by an increasing gap between
+# training and validation dataset performance), we may want to stop the training early, as well.
+
